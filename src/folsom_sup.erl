2be4a2c4ec251c4abec74b678e72f7b16a98e30d
Jordan West <jordanrw@gmail.com>
2011-05-02 07:59:42 -0700

    Nastily remove the web server from folsom. Need to it more elegantly via osenv/app config but I need it gone now.

--- src/folsom_sup.erl
+++ src/folsom_sup.erl
@@ -67,23 +67,23 @@ init([]) ->
 
     Ip = case os:getenv("FOLSOM_IP") of false -> "127.0.0.1"; Any -> Any end,
     Port = case os:getenv("FOLSOM_PORT") of false -> "5565"; Any1 -> Any1 end,
-    LogDir = case os:getenv("FOLSOM_LOG_DIR") of false -> "priv/log"; Any2 -> Any2 end,
-    {ok, Dispatch} = file:consult(filename:join(
-                         [filename:dirname(code:which(?MODULE)),
-                          "..", "priv", "dispatch.conf"])),
-    WebConfig = [
-                 {ip, Ip},
-                 {port, Port},
-                 {log_dir, LogDir},
-                 {dispatch, Dispatch}
-                ],
+%    LogDir = case os:getenv("FOLSOM_LOG_DIR") of false -> "priv/log"; Any2 -> Any2 end,
+%    {ok, Dispatch} = file:consult(filename:join(
+%                         [filename:dirname(code:which(?MODULE)),
+%                          "..", "priv", "dispatch.conf"])),
+%    WebConfig = [
+%                 {ip, Ip},
+%                 {port, Port},
+%                 {log_dir, LogDir},
+%                 {dispatch, Dispatch}
+%                ],
 
-    Web = {webmachine_mochiweb,
-           {webmachine_mochiweb, start, [WebConfig]},
-           permanent,
-           5000,
-           worker,
-           dynamic},
+%    Web = {webmachine_mochiweb,
+%           {webmachine_mochiweb, start, [WebConfig]},
+%           permanent,
+%           5000,
+%           worker,
+%           dynamic},
 
     EventMgr = {folsom_event_manager,
                 {gen_event, start_link, [{local, folsom_event_manager}]},
@@ -91,8 +91,8 @@ init([]) ->
                 brutal_kill,
                 worker,
                 dynamic},
-
-    Processes = [Web, EventMgr],
+    Processes = [EventMgr],
+%    Processes = [Web, EventMgr],
     {ok, { {one_for_one, 10, 10}, Processes} }.
 
 setup_ets_tables() ->
